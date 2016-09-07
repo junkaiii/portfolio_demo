e9b344585e278c6b10c40ec857977c9d6a4bf1d6
Jun Kai Lim <junkaiii@gmail.com>
2016-09-07 16:40:47 +0800

    used TweenMax fromTo to decrease code length but looks slightly unatural

--- script.js
+++ script.js
@@ -4,130 +4,58 @@ $(function() {
   // Init ScrollMagic Controller
   var scrollMagicController = new ScrollMagic.Controller();
 
-  var tweenh1 = TweenMax.to('#h', 0.4, {
-    y: '-=50'});
+  var tweenh = TweenMax.fromTo('#h', 0.3, {y: '+=50'},{y: '-=50', repeat: 1, yoyo: true, delay: 0.2});
 
-  var tweenh2 = TweenMax.to('#h', 0.3, {
-    y: '+=50', delay: 0.4});
-
-
-  var sceneh1 = new ScrollMagic.Scene({
+  var sceneh = new ScrollMagic.Scene({
     triggerElement: '#scene1',
     offset: 450,  /* offset the trigger 150px below #scene's top */
     // duration: 30 /* for 300 pixels down */
   })
-  .setTween(tweenh1)
+  .setTween(tweenh)
   .addIndicators()
   .addTo(scrollMagicController);
 
-  var sceneh2 = new ScrollMagic.Scene({
+  var tweene = TweenMax.fromTo('#e', 0.3, {y: '+=50'},{y: '-=50', repeat: 1, yoyo: true, delay: 0.2});
+
+  var scenee = new ScrollMagic.Scene({
     triggerElement: '#scene1',
-    offset: 455,  /* offset the trigger 150px below #scene's top */
-    // duration: 10 /* for 300 pixels down */
-  })
-  .setTween(tweenh2)
-  .addIndicators()
-  .addTo(scrollMagicController);
-
-  var tweene1 = TweenMax.to('#e', 0.4, {
-    y: '-=50'});
-
-  var tweene2 = TweenMax.to('#e', 0.3, {
-    y: '+=50', delay: 0.4});
-
-
-  var sceneh1 = new ScrollMagic.Scene({
-    triggerElement: '#scene1',
-    offset: 470,  /* offset the trigger 150px below #scene's top */
+    offset: 480,  /* offset the trigger 150px below #scene's top */
     // duration: 30 /* for 300 pixels down */
   })
-  .setTween(tweene1)
+  .setTween(tweene)
   .addIndicators()
   .addTo(scrollMagicController);
 
-  var sceneh2 = new ScrollMagic.Scene({
-    triggerElement: '#scene1',
-    offset: 475,  /* offset the trigger 150px below #scene's top */
-    // duration: 10 /* for 300 pixels down */
-  })
-  .setTween(tweene2)
-  .addIndicators()
-  .addTo(scrollMagicController);
+  var tweenl = TweenMax.fromTo('#l', 0.3, {y: '+=50'},{y: '-=50', repeat: 1, yoyo: true, delay: 0.2});
 
-  var tweenl1 = TweenMax.to('#l', 0.4, {
-    y: '-=50'});
-
-  var tweenl2 = TweenMax.to('#l', 0.3, {
-    y: '+=50', delay: 0.4});
-
-
-  var scenel1 = new ScrollMagic.Scene({
-    triggerElement: '#scene1',
-    offset: 490,  /* offset the trigger 150px below #scene's top */
-    // duration: 30 /* for 300 pixels down */
-  })
-  .setTween(tweenl1)
-  .addIndicators()
-  .addTo(scrollMagicController);
-
-  var scenel2 = new ScrollMagic.Scene({
-    triggerElement: '#scene1',
-    offset: 495,  /* offset the trigger 150px below #scene's top */
-    // duration: 10 /* for 300 pixels down */
-  })
-  .setTween(tweenl2)
-  .addIndicators()
-  .addTo(scrollMagicController);
-
-
-  var tweenl_1 = TweenMax.to('#l_', 0.4, {
-    y: '-=50'});
-
-  var tweenl_2 = TweenMax.to('#l_', 0.3, {
-    y: '+=50', delay: 0.4});
-
-
-  var scenel_1 = new ScrollMagic.Scene({
+  var scenel = new ScrollMagic.Scene({
     triggerElement: '#scene1',
     offset: 510,  /* offset the trigger 150px below #scene's top */
     // duration: 30 /* for 300 pixels down */
   })
-  .setTween(tweenl_1)
+  .setTween(tweenl)
   .addIndicators()
   .addTo(scrollMagicController);
 
-  var scenel_2 = new ScrollMagic.Scene({
+  var tweenl_ = TweenMax.fromTo('#l_', 0.3, {y: '+=50'},{y: '-=50', repeat: 1, yoyo: true, delay: 0.2});
+
+  var scenel_ = new ScrollMagic.Scene({
     triggerElement: '#scene1',
-    offset: 515,  /* offset the trigger 150px below #scene's top */
-    // duration: 10 /* for 300 pixels down */
-  })
-  .setTween(tweenl_2)
-  .addIndicators()
-  .addTo(scrollMagicController);
-
-
-  var tweeno1 = TweenMax.to('#o', 0.4, {
-    y: '-=50'});
-
-  var tweeno2 = TweenMax.to('#o', 0.3, {
-    y: '+=50', delay: 0.4});
-
-
-  var sceneo1 = new ScrollMagic.Scene({
-    triggerElement: '#scene1',
-    offset: 530,  /* offset the trigger 150px below #scene's top */
+    offset: 540,  /* offset the trigger 150px below #scene's top */
     // duration: 30 /* for 300 pixels down */
   })
-  .setTween(tweeno1)
+  .setTween(tweenl_)
   .addIndicators()
   .addTo(scrollMagicController);
 
-  var sceneo2 = new ScrollMagic.Scene({
+  var tweeno = TweenMax.fromTo('#o', 0.3, {y: '+=50'},{y: '-=50', repeat: 1, yoyo: true, delay: 0.2});
+
+  var sceneo = new ScrollMagic.Scene({
     triggerElement: '#scene1',
-    offset: 535,  /* offset the trigger 150px below #scene's top */
-    // duration: 10 /* for 300 pixels down */
+    offset: 570,  /* offset the trigger 150px below #scene's top */
+    // duration: 30 /* for 300 pixels down */
   })
-  .setTween(tweeno2)
+  .setTween(tweeno)
   .addIndicators()
   .addTo(scrollMagicController);
 
